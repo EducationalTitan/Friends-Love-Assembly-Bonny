4f53d0fabf02f66c0a8fb8390fefb68a3e96caac
Wilcox Kingsley Ala <111095726+EducationalTitan@users.noreply.github.com>
2024-11-29 11:45:35 +0100

    Update settings.py

--- friends_love_assembly/settings.py
+++ friends_love_assembly/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(&1ak_0lqp0=05aigvhtn+bv$2jb_^l8lqte#q8+y5a#0c=nk2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["friends-love-assembly.onrender.com",
+ALLOWED_HOSTS = ["friends-love-assembly-Bonny.onrender.com",
     "127.0.0.1",
     "localhost",]
 
